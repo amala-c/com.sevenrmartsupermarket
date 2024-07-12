3e19ae155529faeb7e6d35dbf01d447a2c60c534
Amala Chellappan <Amala Chellappan@TIL0C095>
2024-07-12 07:04:27 +0530

    manage pages chnages commit

--- com.sevenrmartsupermarket/src/main/java/com/sevenrmartsupermarket/pages/ManagePagesPage.java
+++ com.sevenrmartsupermarket/src/main/java/com/sevenrmartsupermarket/pages/ManagePagesPage.java
@@ -1,15 +1,49 @@
 package com.sevenrmartsupermarket.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import com.sevenrmartsupermarket.utilities.GeneralUtility;
 
 public class ManagePagesPage {
 	WebDriver driver;
-	public void ManagepagesPage(WebDriver driver)
+	public ManagePagesPage(WebDriver driver)
 	{
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+	GeneralUtility generalUtility=new GeneralUtility();
+	@FindBy(xpath="//a[@class='btn btn-rounded btn-primary']")
+	WebElement searchButtonTobBar;
+	@FindBy(xpath="//button[@name='Search']")
+	WebElement searchButton;
+	@FindBy(xpath="//input[@class='form-control']")
+	WebElement titleText;
+	@FindBy(xpath="//h4[@class='card-title']")
+	WebElement pageTitleText; 
 	
-
+	public void expandSearchTab() 
+	{
+		searchButtonTobBar.click();
+	}
+	 public void enterTextFieldValue(String TitletextField)
+	 {
+		 titleText.sendKeys(TitletextField);
+	 }
+	 public void clickOnSearchButton()
+	 {
+		 searchButton.click();
+	 }
+	 public void searchEnteredPage(String TtextField) {
+		 expandSearchTab();
+		 enterTextFieldValue(TtextField);
+		 clickOnSearchButton(); 
+	 }
+	
+	 public boolean checkSucessByText(String expectedText)
+	 {
+		 return generalUtility.is_TextPresent(pageTitleText, expectedText);
+	 }
 }
--- com.sevenrmartsupermarket/src/test/java/com/sevenrmartsupermarket/tests/ManagePagesTest.java
+++ com.sevenrmartsupermarket/src/test/java/com/sevenrmartsupermarket/tests/ManagePagesTest.java
@@ -1,17 +1,20 @@
 package com.sevenrmartsupermarket.tests;
 
+import static org.testng.Assert.assertTrue;
+
 import org.testng.annotations.Test;
 
 import com.sevenrmartsupermarket.base.Base;
 import com.sevenrmartsupermarket.pages.AdminUserPage;
 import com.sevenrmartsupermarket.pages.DashBoardPage;
 import com.sevenrmartsupermarket.pages.LoginPage;
+import com.sevenrmartsupermarket.pages.ManagePagesPage;
 
 public class ManagePagesTest extends Base {
 	LoginPage loginpage;
 	DashBoardPage dashBoardPage;
 	AdminUserPage adminuserpage;
-	ManagePagesTest managepagestest;
+	ManagePagesPage managepagespages;
 	
 	@Test
 	public void navigate() {
@@ -20,6 +23,20 @@ public class ManagePagesTest extends Base {
 		loginpage.login();
 		dashBoardPage.navigateToCard("Manage Pages");
 	}
+	
+	@Test
+	public void searchTitle()
+	{
+		loginpage=new LoginPage(driver);
+		dashBoardPage=new DashBoardPage(driver);
+		managepagespages=new ManagePagesPage(driver);
+		loginpage.login();
+		dashBoardPage.navigateToCard("Manage Pages");
+		managepagespages.searchEnteredPage("Preethi");
+		boolean actualText=managepagespages.checkSucessByText("Search List Pages");
+		assertTrue(actualText);
+	}
+	
 	
 	
 }
